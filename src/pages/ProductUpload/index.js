06db6d9bd1db2b042005ca5f313f92af55b234c4
Neelam Makwana <hiyasoni5@gmail.com>
2024-10-09 17:31:10 +0530

    3 commit

--- src/pages/ProductUpload/index.js
+++ src/pages/ProductUpload/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import MenuItem from "@mui/material/MenuItem";
 import Rating from "@mui/material/Rating";
 import Select from "@mui/material/Select";
